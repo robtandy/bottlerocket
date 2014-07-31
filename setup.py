996b330b7f3392f203b3e12749f41dd0cc152152
rob tandy <rob.tandy@gmail.com>
2014-07-31 02:18:20 +0000

    version 0.1.2, uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import bottlerocket 
+
+V =  bottlerocket.__version__
+
+setup(name='bottlerocket',
+      version=V,
+      author='Rob Tandy',
+      author_email='rob.tandy@gmail.com',
+      url='https://github.com/robtandy/bottlerocket',
+      long_description="""
+      Bottlerocket instruments any code using the excellent bottle web 
+      framework (http://www.bottlepy.org) to send timing information 
+      to a running staggregator instance 
+      (https://github.com/robtandy/staggregator) for aggregation and
+      delivery to graphite.""",
+      packages=['bottlerocket'],
+      scripts=['bin/bottlerocket'],
+      install_requires=["pystaggregator >= 0.1.2"],
+)
